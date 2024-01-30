937cba999126609d250cae64592a78146ceca974
daviidarr <nononymous@gmail.com>
2024-01-30 19:47:26 +0100

    modified:   hardhat.config.js 	new file:   package.json 	modified:   scripts/deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,30 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+// require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
 module.exports = {
   solidity: {
     compilers: [{version: "0.8.10"},{version: "0.8.20"}],
     overrides: {
       "@aave/core-v3/contracts/misc/AaveOracle.sol": {version: "0.8.10"}
     }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    },
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_ARBITRUM_URL,
+        blockNumber: 175731373
+      }
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
   }
-}
+};
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,17 +4,28 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0");
+  const asset = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
+  const base = "0x5979D7b546E38E414F7E9822514be443A4800529";
+  const ltv = 0.9;
+  const slippage = 0.0001;
 
-  const looping = await hre.ethers.deployContract("Looping", [unlockTime], {
-    value: lockedAmount,
-  });
+  // const lockedAmount = ethers.utils.parseEther("0.001");
+  const Looping = await ethers.getContractFactory("Looping")
+  const looping = await Looping.deploy(
+      asset,
+      base,
+      ltv,
+      slippage);
+
+  console.log(
+    `contract ${looping.target}`
+  );
 
   await looping.waitForDeployment();
 
